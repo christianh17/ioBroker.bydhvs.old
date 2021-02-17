39f86961a6f2efe513c1d079a78741a83ef337d7
christianh17 <github@familie-herrmann.de>
2021-02-17 19:12:16 +0100

    initial commit

--- admin/words.js
+++ admin/words.js
@@ -14,28 +14,29 @@ systemDictionary = {
         "pl": "Ustawienia adaptera dla bydhvs",
         "zh-cn": "bydhvs的适配器设置"
     },
-    "option1": {
-        "en": "option1",
-        "de": "Option 1",
-        "ru": "Опция 1",
-        "pt": "Opção 1",
-        "nl": "Optie 1",
-        "fr": "Option 1",
-        "it": "opzione 1",
-        "es": "Opción 1",
-        "pl": "opcja 1",
-        "zh-cn": "选项1"
+    "Interval (s)": {
+        "en": "Interval (s)",
+        "de": "Intervall (e)",
+        "ru": "Интервал (с)",
+        "pt": "Intervalo (s)",
+        "nl": "Interval (en)",
+        "fr": "Intervalle (s)",
+        "it": "Intervallo (i)",
+        "es": "Intervalo (s)",
+        "pl": "Odstęp (y)",
+        "zh-cn": "间隔"
     },
-    "option2": {
-        "en": "option2",
-        "de": "Option 2",
-        "ru": "option2",
-        "pt": "opção 2",
-        "nl": "Optie 2",
-        "fr": "Option 2",
-        "it": "opzione 2",
-        "es": "opcion 2",
-        "pl": "Opcja 2",
-        "zh-cn": "选项2"
+
+    "IP Address": {
+        "en": "IP Address",
+        "de": "IP Adresse",
+        "ru": "Айпи адрес",
+        "pt": "Endereço de IP",
+        "nl": "IP adres",
+        "fr": "Adresse IP",
+        "it": "Indirizzo IP",
+        "es": "Dirección IP",
+        "pl": "Adres IP",
+        "zh-cn": "IP地址"
     }
 };
